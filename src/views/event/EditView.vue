b52b916b0b68d9d353dccdf159b9327eaa87801c
KengMaLaJA <kengchayodom@gmail.com>
2025-07-24 22:43:10 +0700

    1.6 Edit button to detail with flash message

--- src/views/event/EditView.vue
+++ src/views/event/EditView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { defineProps, toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
+import { useRouter } from 'vue-router'
 
 const props = defineProps<{
   event: Event
@@ -8,8 +10,22 @@ const props = defineProps<{
 }>()
 
 const { event } = toRefs(props)
+const router = useRouter()
+const store = useMessageStore()
+
+const Edit = () => {
+  // If the registration API call successful
+  // Push back to the event details view
+  store.updateMessage('You successsuflly go to Detail page')
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { od: props.event.id } })
+}
 </script>
 
 <template>
   <p>Edit event here</p>
+
+  <button @click="Edit">GO to detail page</button>
 </template>
